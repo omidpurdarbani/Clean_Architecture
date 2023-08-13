22bec930f3dd40d36706907c9ad74e3961a790ba
Omid purdarbani <omidpurdarbani@gmail.com>
2023-08-13 15:16:36 +0330

    Create IoC project

---
+++ CleanArchitecture/IoC/DependencyContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Services;
+using Data.Repository;
+using Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            #region Application Layer
+
+            service.AddScoped<ICourseService, CourseService>();
+
+            #endregion
+
+            #region Data Layer
+
+            service.AddScoped<ICourseRepository, CourseRepository>();
+
+            #endregion
+        }
+    }
+}
