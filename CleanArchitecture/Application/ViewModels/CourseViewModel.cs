a0f60bbb15317e34f5ebb1181d40392e5b703b98
Omid purdarbani <omidpurdarbani@gmail.com>
2023-08-13 14:33:40 +0330

    Initial Service Course

---
+++ CleanArchitecture/Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class CoursesViewModel
+    {
+        [DisplayName("دوره ها")]
+        public IEnumerable<CourseViewModel> Courses { get; set; }
+    }
+
+    public class CourseViewModel
+    {
+        [DisplayName("کد")]
+        public int Id { get; set; }
+
+        [DisplayName("نام")]
+        public string Name { get; set; }
+
+        [DisplayName("توضیحات")]
+        public string Description { get; set; }
+
+        [DisplayName("عکس")]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ CleanArchitecture/Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        public IEnumerable<Course> GetCourses();
+    }
+}
