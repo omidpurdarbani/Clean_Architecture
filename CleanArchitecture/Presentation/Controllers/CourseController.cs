b56b9a728cd292964355373dde4b44a0af07c9b4
Omid purdarbani <omidpurdarbani@gmail.com>
2023-08-13 16:08:33 +0330

    stable

---
+++ CleanArchitecture/Presentation/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_courseService.GetCourses());
+        }
+    }
+}
